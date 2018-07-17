d8da8a678f9a78874833b1aa50b9f8d1508e7138
Nikolaos Lygkonis <2betafactor@gmail.com>
2018-07-17 17:23:25 +0100

    Minor update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 def hello
-  render html: "hola mundo"
+  render html: "I play real sports. Not trying to be the best at exercising. Fuck this guy. - Kenny Powers"
 end
 def goodbye
     render html: "good goodbye"
